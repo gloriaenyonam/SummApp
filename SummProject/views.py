0d1ca8056857beb362bcc74241b6b625cc7784aa
gloria <enyonamlaryea154@gmail.com>
2023-06-16 17:58:12 +0000

    creating new user

--- SummProject/views.py
+++ SummProject/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from users.models import APPUser
 
@@ -14,12 +14,41 @@ def summary(request):
 def friends_list(request):
     return render(request, 'friends_list.html')
 
-def sign_up(request):
-    # print("method", request.method)
-    # if request.method == "POST":
-    #     email = ''
-    #     APPUser.objects.filter(email=email)
+def sign_up(request):   
+#    print("method", request.method)
+    if request.method == "POST":
+        first_name= request.POST.get("first_name")
+        print("first_name")
+        last_name= request.POST.get("last_name")
+        print("last_name")
+        email= request.POST.get("email_address")
+        print("email")
+        phone_number= request.POST.get("phone_number")
+        print("phone_number")
+        country= request.POST.get("country")
+        print("country")
+        city= request.POST.get("city")
+        print("city")
+        password= request.POST.get("password")
+        print("password")
+        confirm_password= request.POST.get("confirm_password")
+        print("confirm_password")
+        if password != confirm_password:
+            print("passwords don't match")
+            return ""
+        try:
+            APPUser.objects.get(email=email)
+            print("email already exists")
+        except:
+            print("new user")
+            new_user = APPUser.objects.create(email=email, first_name=first_name, last_name=last_name, phone_number=phone_number, country=country, city=city)
+            new_user.set_password(password)
+            new_user.save()
+            return redirect("/log_in/")
+        
+
     return render(request, 'sign_up.html')
+    
 
 def my_account(request):
     return render(request, 'my_account.html')
